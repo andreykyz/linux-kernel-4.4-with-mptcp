658867f8f9fd320c1c6b4832f975bfcb397e0a14
Kyznecov Andrey <andreykyz@gmail.com>
2016-08-30 16:29:16 +0300

    Revert "Merge branch 'tcp-rack'"
    This reverts commit eb9fae328faff9807a4ab5c1834b19f34dd155d4, reversing
    changes made to c8fdc324916a864de753db6de6423b048c20cc0f.
    
    Signed-off-by: Kyznecov Andrey <andreykyz@gmail.com>

--- net/ipv4/tcp_minisocks.c
+++ net/ipv4/tcp_minisocks.c
@@ -460,7 +460,6 @@ struct sock *tcp_create_openreq_child(struct sock *sk, struct request_sock *req,
 
 		newtp->srtt_us = 0;
 		newtp->mdev_us = jiffies_to_usecs(TCP_TIMEOUT_INIT);
-		newtp->rtt_min[0].rtt = ~0U;
 		newicsk->icsk_rto = TCP_TIMEOUT_INIT;
 
 		newtp->packets_out = 0;
@@ -538,8 +537,6 @@ struct sock *tcp_create_openreq_child(struct sock *sk, struct request_sock *req,
 		tcp_ecn_openreq_child(newtp, req);
 		newtp->fastopen_rsk = NULL;
 		newtp->syn_data_acked = 0;
-		newtp->rack.mstamp.v64 = 0;
-		newtp->rack.advanced = 0;
 
 		newtp->saved_syn = req->saved_syn;
 		req->saved_syn = NULL;
